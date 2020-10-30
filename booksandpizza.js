0900884ec7354ef76946effed8d253ce646672d3
apears2020 <67842332+apears2020@users.noreply.github.com>
2020-10-30 09:58:52 -0500

    update

--- booksandpizza.js
+++ booksandpizza.js
@@ -10,14 +10,29 @@ $(document).ready(
             var lateBooks= $("#lateBooksAmount").val();
             lateBooks= parseFloat(lateBooks);
             var lateDVDsDays = $("#lateDaysDVDs").val();
-            lateDVDsDays = $
+            lateDVDsDays = parseFloat(lateDVDsDays);
             var lateDVDsAmount = $("#lateDVDAmount").val();
+            lateDVDsAmount = parseFloat(lateDVDsAmount);
+
+            var lateFeesAllBooks = ((lateBooksDays * lateBooks * 0.25) + (lateDVDsDays * lateDVDsAmount * 0.50));
+
+            $("#bookP").append("Your library fees are $" + (lateFeesAllBooks));
 
         }
+
         function pizzaTime(){
             var pizzaAmount = $("#howManyPizzas").val();
+            pizzaAmount = parseFloat(pizzaAmount);
             var toppingAmount = $("#howManyToppings").val();
+            toppingAmount = parseFloat(toppingAmount);
             var crewAmount = $("#howManyCrew").val();
+            crewAmount = parseFloat(crewAmount);
+
+            var pizzasOrdered = (pizzaAmount * 15.00);
+            var toppingsOrdered = (toppingAmount * 1.25);
+
+            var pizzasForAll = ((pizzasOrdered + toppingsOrdered)/crewAmount);
+            $("#pizzaP").append("Your pizza order will cost $" +pizzasForAll+ " per person.");
         }
     }
 );
